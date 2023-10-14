9aac9d2198172ca08a36627c22a858a0e6ba7c6d
earezza <>
2023-10-14 14:58:03 -0400

    updated V2

--- kijiji_rentals_process.py
+++ kijiji_rentals_process.py
@@ -39,45 +39,6 @@ parser.add_argument('--city', help='City to default to for long/lat coordinates 
 parser.add_argument('--country', help='Country to default to for long/lat coordinates if ad location is unsearchable', type=str, default="Canada")
 args = parser.parse_args()
 
-'''
-beds_map = {'1 + Den': '2', '2 + Den': '3', 
-           '3 + Den': '4', '4 + Den': '5', 
-           '5 + Den': '6', '6 + Den': '7',
-           '7 + Den': '8', 'Bachelor/Studio': '1',
-           '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8'
-           }
-
-number_map = {'zero': '0', 'one': '1', 'two': '2', 'three': '3',
-              'four': '4', 'five': '5', 'six': '6', 'seven': '7',
-              'eight': '8', 'nine': '9', 'ten': '10'}
-
-price_not_listed = ['Please Contact', 'Swap / Trade', 'Free']
-
-
-def get_num_from_ambiguities(df, column, pattern, limit=10):
-    df_temp = df.copy()
-    colsearch = df_temp[column].str.lower()
-    # Get number before/after pattern
-    temp = colsearch.str.split(pattern, expand=True) # number typically in 0th column (e.g. 1bd)
-    temp = temp[0]
-    temp_list = temp.str.split()
-    # Get number (would be in last word/number in 0 column)
-    vals = pd.Series([ temp_list.loc[i][-1] if len(temp_list.loc[i]) > 0 else np.nan for i in temp_list.index.values ])
-    vals.index = colsearch.index
-    vals = vals.dropna()
-    vals = vals[vals.str.isdigit()].astype(int)
-    vals = vals[vals < limit]
-    
-    return vals
-
-def get_ads_with_pattern(df, column, pattern):
-    df_temp = df.copy()
-    colsearch = df_temp[column].str.lower()
-    
-    return df_temp[colsearch.str.contains(pattern)]
-'''
-
-
 def get_coordinates(df):
     geolocator = Nominatim(user_agent="kijiji", timeout=3)
     geocode = RateLimiter(geolocator.geocode,
@@ -114,7 +75,7 @@ def get_coordinates(df):
 
 def anonymize_values(series):
     mapping = dict(zip(series.unique(), range(0, len(series.unique()))))
-    series = series.map(mapping).astype('int32')
+    series = series.map(mapping).astype('uint32')
     return series
     
 # pd.DataFrame(columns=["Title", "Price", "Location", "Description", "PostingDate", "Poster", "AdURL", "AdId", "ScrapeDate", "Longitude", "Latitude"])
@@ -136,7 +97,22 @@ if __name__ == '__main__':
         df_cleaned = pd.read_csv(args.output_file)
     else:
         print("Writing cleaned data to %s"%args.output_file)
-        
+    
+    
+    # Categorize rental ad by listing group
+    urls_expanded = df['AdURL'].str.split('/', expand=True)
+    df['RentalCategory'] = urls_expanded[3]
+    category_mapping = {'v-apartments-condos': 'Apartments-Condos',
+                        'v-commercial-office-space': 'Commercial-Office-Space',
+                        'v-room-rental-roommate': 'Room-Rental-Roommate',
+                        'v-short-term-rental': 'Short-Term-Rental',
+                        'v-storage-parking': 'Storage-Parking'}
+    df['RentalCategory'] = df['RentalCategory'].map(category_mapping)
+    
+    # Add columns to distinguish rental type
+    df['Commercial'] = (df['RentalCategory'] == 'Commercial-Office-Space').astype('uint8')
+    df['Residential'] = (df['RentalCategory'].isin(['Apartments-Condos', 'Room-Rental-Roommate', 'Short-Term-Rental'])).astype('uint8')
+    
     # Assign Poster with anonymized IDs
     df['Poster'] = anonymize_values(df['Poster'])
     
@@ -158,11 +134,10 @@ if __name__ == '__main__':
     df['PostingDate'] = pd.to_datetime(df['PostingDate'], format='%Y-%m-%dT%H:%M:%S', utc=True)
     
     # Modify binary values
-    for c in df.columns:
-        print(c)
-        if 'Yes' in df[c].values or 'No' in df[c].values:
-            print('\tModifying...\n')
-            df[c] = df[c].map({'Yes': 1, 'No': 0}, na_action='ignore').astype('Int16')
+    for c in df.columns[df.dtypes == 'object']:
+        if 'Yes' in df[c].unique() or 'No' in df[c].unique():
+            df[c].replace(to_replace='Not-Available', value='No', inplace=True)
+            df[c] = df[c].map({'Yes': 1, 'No': 0}, na_action='ignore').astype('float16')
     
     # Format Move-In-Date
     if 'Move-In-Date' in df.columns:
@@ -171,6 +146,7 @@ if __name__ == '__main__':
     # Format number of Parking-Included
     if 'Parking-Included' in df.columns:
         df['Parking-Included'].replace(to_replace='Not-Available', value='0', inplace=True)
+        df['Parking-Included'].replace(to_replace='No', value='0', inplace=True)
         df['Parking-Included'] = df['Parking-Included'].str.extract(r'(\d+)')
         df['Parking-Included'] = df['Parking-Included'].astype('float16')
         
@@ -185,117 +161,45 @@ if __name__ == '__main__':
     df['Longitude'] = df['Longitude'].astype('float32')
     df['Latitude'] = df['Latitude'].astype('float32')
     
-    
-    
-    # Extract specific rental info from description
+    '''
+    # ------------------- Difficult Part ------------------
+    # Extract specific rental info from title and description text
+    df['Title'] = df['Title'].str.lower()
     df['Description'] = df['Description'].str.lower()
+    text = (df['Title'] + df['Description']).str.replace('\n', '')
+    
+    # Keywords to search
+    girls = ['girl', 'female', 'woman', 'women', 'lady', 'ladies']
+    boys = ['boy', 'male', 'man', 'men', 'guy', 'dude']
+    sublet = ['sublet']
+    students = ['student']
+    preference = ["prefer", "only"]
     
     
+    # Use regex
+    text.str.extract(r'(\S+)')
     
     
+    # Find Preferene-Male
+    df['Preference-Male'] = ( text.str.contains('|'.join(boys)) & (~text.str.contains('|'.join(girls))) )
     
+    # Find Preference-Female
+    df['Preference-Feale'] = ( (text.str.contains('|'.join(girls))) )
     
+    # Find Sublets
+    df['Sublet'] = ( (text.str.contains('|'.join(sublet))) )
     
+    # Find Students
+    df['Students'] = ( (text.str.contains('|'.join(students))) )
+    
+    # Find Preference-Other
+    df['Preference'] = ( (text.str.contains('|'.join(preference))) )
+    
+    text[text.str.contains('|'.join(girls))]
+    text[( text.str.contains('|'.join(boys)) ) & (~text.str.contains('|'.join(girls)))]
     '''
-    # Fill in number of rooms from raw data and searching ad title + description 
-    print("Filling in number of rooms info...")
-    bed_strings = ['bd', 'bed', 'room', 'bdr', 'bedr', 'bedrm', 'bdrm', 'bdroom', 'bedroom']
-    no_beds = df[df['Bedrooms'].isna()]
     
-    # Look in title and description for info
-    queries = []
-    for p in bed_strings:
-        queries.append(get_num_from_ambiguities(no_beds, 'Title', p, limit=8))
-        queries.append(get_num_from_ambiguities(no_beds, 'Description', p, limit=8))
-    
-    # Consolidate queries to select most represented number of rooms for each ad
-    num_beds = pd.concat(queries, axis=1)
-    num_beds = num_beds.mode(axis=1, dropna=True)
-    num_beds_unambiguous = num_beds[num_beds.notna().sum(axis=1) == 1][0].astype(int)
-    #num_beds_ambiguous = num_beds[num_beds.notna().sum(axis=1) > 1]
-    
-    # Add column to df to include number of rooms found by original df, or by searching title and description
-    df['NumberOfRooms'] = df['Bedrooms'].map(beds_map)
-    df['NumberOfRooms'].loc[num_beds_unambiguous.index] = num_beds_unambiguous
-    df['NumberOfRooms'] = df['NumberOfRooms'].fillna(0).astype(int)
-    df['NumberOfRooms'] = df['NumberOfRooms'].replace(0, np.nan)
-    
-    # Get ads without prices and then remove
-    df_no_price = df[df['Price'].isin(price_not_listed)]
-    
-    print("Removing ads with no price listed...")
-    # Indicate ads with no price
-    has_price = df[~df['Price'].isin(price_not_listed)]
-    df['PriceAvailable'] = df['Price']
-    df['PriceAvailable'] = df.index.isin(has_price.index)
-    
-    
-    # Use average for ads with multiple prices
-    df['Price'] = df['Price'].str.replace('Please Contact', '')
-    df['Price'] = df['Price'].str.replace('Swap / Trade', '')
-    df['Price'] = df['Price'].str.replace('Free', '')
-    prices = df['Price'].str.split(expand=True)
-    prices = prices.astype(float)
-    prices = prices.mean(axis=1)
-    df['RentPrice'] = df['Price']
-    df['RentPrice'].loc[prices.index] = prices
-    df['RentPrice'] = df['RentPrice'].astype(float)
-    
-    sublets = pd.DataFrame()
-    sublets = pd.concat([sublets, get_ads_with_pattern(df, 'Title', 'sublet')])
-    sublets = pd.concat([sublets, get_ads_with_pattern(df, 'Description', 'sublet')])
-    sublets.drop_duplicates(inplace=True)
-    df['Sublet'] = df.index.isin(sublets.index)
-    
-    print("Collecting student-only rentals...")
-    student_strings = ['student only', 'students only', 'student-only', 'students-only', 'all student', 'all students',
-                   'for student', 'for students', 'student only', 'students only', 'student-only', 'students-only', 'all student', 'all students',
-                   'student preferred', 'students preferred', 'student preferred', 'students preferred']
-    
-    # Look in title and description for info
-    students = pd.DataFrame()
-    for p in student_strings:
-        students = pd.concat([students, get_ads_with_pattern(df, 'Title', p)])
-        students.drop_duplicates(inplace=True)
-        students = pd.concat([students, get_ads_with_pattern(df, 'Description', p)])
-        students.drop_duplicates(inplace=True)
-        
-    df['StudentOnly'] = df.index.isin(students.index)
-    
-    print("Collecting female-only rentals...")
-    girl_strings = ['for female', 'for females', 'female only', 'females only', 'female-only', 'females-only', 'all female', 'all females',
-                   'for girl', 'for girls', 'girl only', 'girls only', 'girl-only', 'girls-only', 'all girl', 'all girls',
-                   'female preferred', 'females preferred', 'girl preferred', 'girls preferred',
-                   'lady', 'ladies']
-    
-    # Look in title and description for info
-    girls = pd.DataFrame()
-    for p in girl_strings:
-        girls = pd.concat([girls, get_ads_with_pattern(df, 'Title', p)])
-        girls.drop_duplicates(inplace=True)
-        girls = pd.concat([girls, get_ads_with_pattern(df, 'Description', p)])
-        girls.drop_duplicates(inplace=True)
-        
-    df['FemaleOnly'] = df.index.isin(girls.index)
-        
-    print("Collecting male-only rentals...")
-    guy_strings = ['for male', 'for males', ' male only', ' males only', 'male-only', 'males-only', 'all male', 'all males',
-                   'for boy', 'for boys', 'boy only', 'boys only', 'boy-only', 'boys-only', 'all boy', 'all boys',
-                   ' male preferred', ' males preferred', 'boy preferred', 'boys preferred',
-                   ' man', ' men']
-    
-    # Look in title and description for info
-    guys = pd.DataFrame()
-    for p in guy_strings:
-        guys = pd.concat([guys, get_ads_with_pattern(df, 'Title', p)])
-        guys.drop_duplicates(inplace=True)
-        guys = pd.concat([guys, get_ads_with_pattern(df, 'Description', p)])
-        guys.drop_duplicates(inplace=True)
-        
-    df['MaleOnly'] = df.index.isin(guys.index)
-    
-    df['PricePerRoom'] = df['RentPrice'] / df['NumberOfRooms']
-    '''
+    # Write final data to file
     print("Writing to file...")
     df.to_csv(args.output_file, sep=',', header=True, index=False)
     print("Done!")
